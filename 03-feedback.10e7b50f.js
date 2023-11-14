39877cc233866b7a80f44fc98e57d9ef5d74ea5f
Alex11SW <136641877+Alex11SW@users.noreply.github.com>
2023-11-14 13:37:31 +0000

    Deploying to gh-pages from @ Alex11SW/goit-js-hw-08@2148fa62752d530b50db786a3fccbf71a523d647 🚀

--- 03-feedback.10e7b50f.js
+++ 03-feedback.10e7b50f.js
@@ -1,4 +1,4 @@
-!function(){var t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},e={};e=/**
+!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t={};t=/**
  * Checks if `value` is the
  * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
  * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
@@ -22,38 +22,38 @@
  *
  * _.isObject(null);
  * // => false
- */function(t){var e=typeof t;return null!=t&&("object"==e||"function"==e)};var n={},o={},r={};r="object"==typeof t&&t&&t.Object===Object&&t;/** Detect free variable `self`. */var l="object"==typeof self&&self&&self.Object===Object&&self;o=r||l||Function("return this")();var a={},i={},c={},f=/\s/;c=/**
+ */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var n={},o={},r={};r="object"==typeof e&&e&&e.Object===Object&&e;/** Detect free variable `self`. */var l="object"==typeof self&&self&&self.Object===Object&&self;o=r||l||Function("return this")();var a={},c={},i={},f=/\s/;i=/**
  * Used by `_.trim` and `_.trimEnd` to get the index of the last non-whitespace
  * character of `string`.
  *
  * @private
  * @param {string} string The string to inspect.
  * @returns {number} Returns the index of the last non-whitespace character.
- */function(t){for(var e=t.length;e--&&f.test(t.charAt(e)););return e};/** Used to match leading whitespace. */var u=/^\s+/;i=/**
+ */function(e){for(var t=e.length;t--&&f.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var u=/^\s+/;c=/**
  * The base implementation of `_.trim`.
  *
  * @private
  * @param {string} string The string to trim.
  * @returns {string} Returns the trimmed string.
- */function(t){return t?t.slice(0,c(t)+1).replace(u,""):t};var b={},d={},v={};v=o.Symbol;var s={},y=Object.prototype,p=y.hasOwnProperty,g=y.toString,j=v?v.toStringTag:void 0;s=/**
+ */function(e){return e?e.slice(0,i(e)+1).replace(u,""):e};var s={},d={},m={};m=o.Symbol;var v={},b=Object.prototype,g=b.hasOwnProperty,y=b.toString,p=m?m.toStringTag:void 0;v=/**
  * A specialized version of `baseGetTag` which ignores `Symbol.toStringTag` values.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the raw `toStringTag`.
- */function(t){var e=p.call(t,j),n=t[j];try{t[j]=void 0;var o=!0}catch(t){}var r=g.call(t);return o&&(e?t[j]=n:delete t[j]),r};var m={},O=Object.prototype.toString;m=/**
+ */function(e){var t=g.call(e,p),n=e[p];try{e[p]=void 0;var o=!0}catch(e){}var r=y.call(e);return o&&(t?e[p]=n:delete e[p]),r};var j={},O=Object.prototype.toString;j=/**
  * Converts `value` to a string using `Object.prototype.toString`.
  *
  * @private
  * @param {*} value The value to convert.
  * @returns {string} Returns the converted string.
- */function(t){return O.call(t)};/** Built-in value references. */var S=v?v.toStringTag:void 0;d=/**
+ */function(e){return O.call(e)};/** Built-in value references. */var S=m?m.toStringTag:void 0;d=/**
  * The base implementation of `getTag` without fallbacks for buggy environments.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the `toStringTag`.
- */function(t){return null==t?void 0===t?"[object Undefined]":"[object Null]":S&&S in Object(t)?s(t):m(t)};var h={};h=/**
+ */function(e){return null==e?void 0===e?"[object Undefined]":"[object Null]":S&&S in Object(e)?v(e):j(e)};var h={};h=/**
  * Checks if `value` is object-like. A value is object-like if it's not `null`
  * and has a `typeof` result of "object".
  *
@@ -76,7 +76,7 @@
  *
  * _.isObjectLike(null);
  * // => false
- */function(t){return null!=t&&"object"==typeof t},b=/**
+ */function(e){return null!=e&&"object"==typeof e},s=/**
  * Checks if `value` is classified as a `Symbol` primitive or object.
  *
  * @static
@@ -92,6 +92,6 @@
  *
  * _.isSymbol('abc');
  * // => false
- */function(t){return"symbol"==typeof t||h(t)&&"[object Symbol]"==d(t)};/** Used as references for various `Number` constants. */var w=0/0,T=/^[-+]0x[0-9a-f]+$/i,E=/^0b[01]+$/i,$=/^0o[0-7]+$/i,I=parseInt;let N={formElem:document.querySelector(".feedback-form")};N.formElem.addEventListener("input",function(t){let e=t.target.name,n=t.target.value;localStorage.setItem(e,JSON.stringify(n))}),onload()}();//# sourceMappingURL=03-feedback.98e01be9.js.map
+ */function(e){return"symbol"==typeof e||h(e)&&"[object Symbol]"==d(e)};/** Used as references for various `Number` constants. */var E=0/0,w=/^[-+]0x[0-9a-f]+$/i,T=/^0b[01]+$/i,I=/^0o[0-7]+$/i,L=parseInt;function N(e){let t=localStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}}let $={formElem:document.querySelector(".feedback-form")};$.formElem.addEventListener("input",function(e){let t=e.target.name,n=e.target.value;localStorage.setItem(t,JSON.stringify(n))}),document.addEventListener("DOMContentLoaded",function(){let e=N("email"),t=N("message");console.log($.formElem.elements),$.formElem.elements.email.value=e,$.formElem.elements.message.value=t})}();//# sourceMappingURL=03-feedback.10e7b50f.js.map
 
-//# sourceMappingURL=03-feedback.98e01be9.js.map
+//# sourceMappingURL=03-feedback.10e7b50f.js.map
