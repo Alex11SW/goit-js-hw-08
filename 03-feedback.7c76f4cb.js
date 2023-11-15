5b835775c62120034a93b3670a6f7633268568c7
Alex11SW <136641877+Alex11SW@users.noreply.github.com>
2023-11-15 09:44:37 +0000

    Deploying to gh-pages from @ Alex11SW/goit-js-hw-08@840076bec078bc4cd304fba2ad6dd86aaa2f1c60 🚀

--- 03-feedback.7c76f4cb.js
+++ 03-feedback.7c76f4cb.js
@@ -22,38 +22,38 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
  *
  * _.isObject(null);
  * // => false
- */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var n={},o={},r={};r="object"==typeof e&&e&&e.Object===Object&&e;/** Detect free variable `self`. */var l="object"==typeof self&&self&&self.Object===Object&&self;o=r||l||Function("return this")();var a={},c={},i={},f=/\s/;i=/**
+ */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var n={},o={},r={};r="object"==typeof e&&e&&e.Object===Object&&e;/** Detect free variable `self`. */var l="object"==typeof self&&self&&self.Object===Object&&self;o=r||l||Function("return this")();var a={},i={},f={},c=/\s/;f=/**
  * Used by `_.trim` and `_.trimEnd` to get the index of the last non-whitespace
  * character of `string`.
  *
  * @private
  * @param {string} string The string to inspect.
  * @returns {number} Returns the index of the last non-whitespace character.
- */function(e){for(var t=e.length;t--&&f.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var u=/^\s+/;c=/**
+ */function(e){for(var t=e.length;t--&&c.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var u=/^\s+/;i=/**
  * The base implementation of `_.trim`.
  *
  * @private
  * @param {string} string The string to trim.
  * @returns {string} Returns the trimmed string.
- */function(e){return e?e.slice(0,i(e)+1).replace(u,""):e};var s={},d={},m={};m=o.Symbol;var v={},b=Object.prototype,g=b.hasOwnProperty,y=b.toString,p=m?m.toStringTag:void 0;v=/**
+ */function(e){return e?e.slice(0,f(e)+1).replace(u,""):e};var s={},m={},d={};d=o.Symbol;var v={},b=Object.prototype,g=b.hasOwnProperty,p=b.toString,y=d?d.toStringTag:void 0;v=/**
  * A specialized version of `baseGetTag` which ignores `Symbol.toStringTag` values.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the raw `toStringTag`.
- */function(e){var t=g.call(e,p),n=e[p];try{e[p]=void 0;var o=!0}catch(e){}var r=y.call(e);return o&&(t?e[p]=n:delete e[p]),r};var j={},O=Object.prototype.toString;j=/**
+ */function(e){var t=g.call(e,y),n=e[y];try{e[y]=void 0;var o=!0}catch(e){}var r=p.call(e);return o&&(t?e[y]=n:delete e[y]),r};var j={},O=Object.prototype.toString;j=/**
  * Converts `value` to a string using `Object.prototype.toString`.
  *
  * @private
  * @param {*} value The value to convert.
  * @returns {string} Returns the converted string.
- */function(e){return O.call(e)};/** Built-in value references. */var S=m?m.toStringTag:void 0;d=/**
+ */function(e){return O.call(e)};/** Built-in value references. */var S=d?d.toStringTag:void 0;m=/**
  * The base implementation of `getTag` without fallbacks for buggy environments.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the `toStringTag`.
- */function(e){return null==e?void 0===e?"[object Undefined]":"[object Null]":S&&S in Object(e)?v(e):j(e)};var h={};h=/**
+ */function(e){return null==e?void 0===e?"[object Undefined]":"[object Null]":S&&S in Object(e)?v(e):j(e)};var E={};E=/**
  * Checks if `value` is object-like. A value is object-like if it's not `null`
  * and has a `typeof` result of "object".
  *
@@ -92,6 +92,11 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
  *
  * _.isSymbol('abc');
  * // => false
- */function(e){return"symbol"==typeof e||h(e)&&"[object Symbol]"==d(e)};/** Used as references for various `Number` constants. */var E=0/0,w=/^[-+]0x[0-9a-f]+$/i,T=/^0b[01]+$/i,I=/^0o[0-7]+$/i,L=parseInt;function N(e){let t=localStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}}const $={formElem:document.querySelector(".feedback-form")};$.formElem.addEventListener("input",function(e){let t=e.target.name,n=e.target.value;localStorage.setItem(t,JSON.stringify(n))}),document.addEventListener("DOMContentLoaded",function(){let e=N("email"),t=N("message");console.log($.formElem.elements),$.formElem.elements.email.value=e,$.formElem.elements.message.value=t});//# sourceMappingURL=03-feedback.a76e093c.js.map
+ */function(e){return"symbol"==typeof e||E(e)&&"[object Symbol]"==m(e)};/** Used as references for various `Number` constants. */var h=0/0,w=/^[-+]0x[0-9a-f]+$/i,L=/^0b[01]+$/i,T=/^0o[0-7]+$/i,k=parseInt;function I(e,t){localStorage.setItem(e,JSON.stringify(t))}const N={formElem:document.querySelector(".feedback-form")};N.formElem.addEventListener("input",function(e){let t=e.target.name,n=e.target.value;I(t,n)}),N.formElem.addEventListener("submit",function(e){e.preventDefault();let t=N.formElem.elements.email.value,n=N.formElem.elements.message.value;I("feedback-form-state",{email:t,message:n}),console.log({email:t,message:n}),e.target.reset();// localStorage.removeItem('email');
+// localStorage.removeItem('message');
+}),document.addEventListener("DOMContentLoaded",function(){// const email = loadFromLS('email');
+// const message = loadFromLS('message');
+// console.log(refs.formElem.elements);
+let e=function(e){let t=localStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}}("feedback-form-state");e&&(N.formElem.elements.email.value=email||"email",N.formElem.elements.message.value=message||"input text")});//# sourceMappingURL=03-feedback.7c76f4cb.js.map
 
-//# sourceMappingURL=03-feedback.a76e093c.js.map
+//# sourceMappingURL=03-feedback.7c76f4cb.js.map
