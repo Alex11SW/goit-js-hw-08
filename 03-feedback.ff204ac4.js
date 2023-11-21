4c0a3042a980fd60e3206d6a0e7aa1283f4f745c
Alex11SW <136641877+Alex11SW@users.noreply.github.com>
2023-11-21 16:29:55 +0000

    Deploying to gh-pages from @ Alex11SW/goit-js-hw-08@b32e8c0e3841a584ce6a9e796f67172e1234e6e8 🚀

--- 03-feedback.ff204ac4.js
+++ 03-feedback.ff204ac4.js
@@ -22,38 +22,38 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
  *
  * _.isObject(null);
  * // => false
- */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var n={},l={},o={};o="object"==typeof e&&e&&e.Object===Object&&e;/** Detect free variable `self`. */var r="object"==typeof self&&self&&self.Object===Object&&self;l=o||r||Function("return this")();var a={},f={},i={},c=/\s/;i=/**
+ */function(e){var t=typeof e;return null!=e&&("object"==t||"function"==t)};var n={},o={},r={};r="object"==typeof e&&e&&e.Object===Object&&e;/** Detect free variable `self`. */var l="object"==typeof self&&self&&self.Object===Object&&self;o=r||l||Function("return this")();var a={},f={},i={},c=/\s/;i=/**
  * Used by `_.trim` and `_.trimEnd` to get the index of the last non-whitespace
  * character of `string`.
  *
  * @private
  * @param {string} string The string to inspect.
  * @returns {number} Returns the index of the last non-whitespace character.
- */function(e){for(var t=e.length;t--&&c.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var m=/^\s+/;f=/**
+ */function(e){for(var t=e.length;t--&&c.test(e.charAt(t)););return t};/** Used to match leading whitespace. */var s=/^\s+/;f=/**
  * The base implementation of `_.trim`.
  *
  * @private
  * @param {string} string The string to trim.
  * @returns {string} Returns the trimmed string.
- */function(e){return e?e.slice(0,i(e)+1).replace(m,""):e};var s={},u={},d={};d=l.Symbol;var v={},b=Object.prototype,g=b.hasOwnProperty,p=b.toString,y=d?d.toStringTag:void 0;v=/**
+ */function(e){return e?e.slice(0,i(e)+1).replace(s,""):e};var u={},m={},d={};d=o.Symbol;var b={},v=Object.prototype,g=v.hasOwnProperty,p=v.toString,y=d?d.toStringTag:void 0;b=/**
  * A specialized version of `baseGetTag` which ignores `Symbol.toStringTag` values.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the raw `toStringTag`.
- */function(e){var t=g.call(e,y),n=e[y];try{e[y]=void 0;var l=!0}catch(e){}var o=p.call(e);return l&&(t?e[y]=n:delete e[y]),o};var j={},E=Object.prototype.toString;j=/**
+ */function(e){var t=g.call(e,y),n=e[y];try{e[y]=void 0;var o=!0}catch(e){}var r=p.call(e);return o&&(t?e[y]=n:delete e[y]),r};var j={},O=Object.prototype.toString;j=/**
  * Converts `value` to a string using `Object.prototype.toString`.
  *
  * @private
  * @param {*} value The value to convert.
  * @returns {string} Returns the converted string.
- */function(e){return E.call(e)};/** Built-in value references. */var O=d?d.toStringTag:void 0;u=/**
+ */function(e){return O.call(e)};/** Built-in value references. */var S=d?d.toStringTag:void 0;m=/**
  * The base implementation of `getTag` without fallbacks for buggy environments.
  *
  * @private
  * @param {*} value The value to query.
  * @returns {string} Returns the `toStringTag`.
- */function(e){return null==e?void 0===e?"[object Undefined]":"[object Null]":O&&O in Object(e)?v(e):j(e)};var S={};S=/**
+ */function(e){return null==e?void 0===e?"[object Undefined]":"[object Null]":S&&S in Object(e)?b(e):j(e)};var E={};E=/**
  * Checks if `value` is object-like. A value is object-like if it's not `null`
  * and has a `typeof` result of "object".
  *
@@ -76,7 +76,7 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
  *
  * _.isObjectLike(null);
  * // => false
- */function(e){return null!=e&&"object"==typeof e},s=/**
+ */function(e){return null!=e&&"object"==typeof e},u=/**
  * Checks if `value` is classified as a `Symbol` primitive or object.
  *
  * @static
@@ -92,6 +92,6 @@ var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"u
  *
  * _.isSymbol('abc');
  * // => false
- */function(e){return"symbol"==typeof e||S(e)&&"[object Symbol]"==u(e)};/** Used as references for various `Number` constants. */var h=0/0,w=/^[-+]0x[0-9a-f]+$/i,k=/^0b[01]+$/i,L=/^0o[0-7]+$/i,T=parseInt;function I(e,t){localStorage.setItem(e,JSON.stringify(t))}const N={formElem:document.querySelector(".feedback-form")};N.formElem.addEventListener("input",function(e){let t=N.formElem.elements.email.value,n=N.formElem.elements.message.value;I("feedback-form-state",{email:t,message:n})}),N.formElem.addEventListener("submit",function(e){e.preventDefault();let t=N.formElem.elements.email.value,n=N.formElem.elements.message.value;t&&n?(console.log({email:t,message:n}),e.target.reset(),I("feedback-form-state",{})):alert("Please fill in all fields before submitting.")}),document.addEventListener("DOMContentLoaded",function(){let e=function(e){let t=localStorage.getItem(e);try{return JSON.parse(t)}catch(e){return t}}("feedback-form-state")||{};N.formElem.elements.email.value=e.email||"email",N.formElem.elements.message.value=e.message||"input text"}//# sourceMappingURL=03-feedback.fe163579.js.map
+ */function(e){return"symbol"==typeof e||E(e)&&"[object Symbol]"==m(e)};/** Used as references for various `Number` constants. */var h=0/0,w=/^[-+]0x[0-9a-f]+$/i,k=/^0b[01]+$/i,L=/^0o[0-7]+$/i,T=parseInt;function I(e,t){localStorage.setItem(e,JSON.stringify(t))}const N={formElem:document.querySelector(".feedback-form")};N.formElem.addEventListener("input",function(e){let t=N.formElem.elements.email.value,n=N.formElem.elements.message.value;I("feedback-form-state",{email:t,message:n})}),N.formElem.addEventListener("submit",function(e){e.preventDefault();let t=N.formElem.elements.email.value,n=N.formElem.elements.message.value;t&&n?(console.log({email:t,message:n}),e.target.reset(),I("feedback-form-state",{})):alert("Please fill in all fields before submitting.")}),document.addEventListener("DOMContentLoaded",function(){!function(e){let t=localStorage.getItem(e);try{JSON.parse(t)}catch(e){return t}}("feedback-form-state")}//# sourceMappingURL=03-feedback.ff204ac4.js.map
 );
-//# sourceMappingURL=03-feedback.fe163579.js.map
+//# sourceMappingURL=03-feedback.ff204ac4.js.map
